4c294dd7c50790edc529151e9b7f4e7dfb4ca0ae
Scotty Loveless <scottyloveless@icloud.com>
2026-04-11 17:12:56 -0500

    first flake attempt

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
 
         home-manager = {
             url = "github:nix-community/home-manager";
-            follows = "nixpkgs";
+            inputs.nixpkgs.follows = "nixpkgs";
           };
 
         hyprland.url = "github:hyprwm/Hyprland";
