fa26e4880a00765b717421146755fa43e1496390
Scotty Loveless <scottyloveless@icloud.com>
2026-04-11 23:15:14 -0500

    typo before resize_on_border

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -25,7 +25,7 @@
                 border_size = 2;
                 
                 col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg;
-                col.inactive_border = rgba(595959aa)
+                col.inactive_border = rgba(595959aa);
 
                 resize_on_border = false;
 
