530faea155470c44449f564b8a9e5a16c9427da1
Scotty Loveless <scottyloveless@icloud.com>
2026-04-11 23:48:28 -0500

    removed ;

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -97,11 +97,11 @@
               enabled = "yes";
 
               bezier = [
-                "easeOutQuint,   0.23, 1,    0.32, 1";
-                "easeInOutCubic, 0.65, 0.05, 0.36, 1";
-                "linear,         0,    0,    1,    1";
-                "almostLinear,   0.5,  0.5,  0.75, 1";
-                "quick,          0.15, 0,    0.1,  1";
+                "easeOutQuint,   0.23, 1,    0.32, 1"
+                "easeInOutCubic, 0.65, 0.05, 0.36, 1"
+                "linear,         0,    0,    1,    1"
+                "almostLinear,   0.5,  0.5,  0.75, 1"
+                "quick,          0.15, 0,    0.1,  1"
               ];
                 animation = [
                   "# Default animations, see https://wiki.hypr.land/Configuring/Animations/"
