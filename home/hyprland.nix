18ff73172f17982a39a057cdcc11517ee4fef672
Scotty Loveless <scottyloveless@icloud.com>
2026-04-11 23:22:41 -0500

    more hyprland typos

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -24,8 +24,8 @@
                 gaps_out = 20;
                 border_size = 2;
                 
-                col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg;
-                col.inactive_border = rgba(595959aa);
+                col.active_border = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+                col.inactive_border = "rgba(595959aa)";
 
                 resize_on_border = false;
 
@@ -48,7 +48,7 @@
                   enabled = true;
                   range = 4;
                   render_power = 3;
-                  color = rgba(1a1a1aee);
+                  color = "rgba(1a1a1aee)";
               };
 
               # https://wiki.hypr.land/Configuring/Variables/#blur
