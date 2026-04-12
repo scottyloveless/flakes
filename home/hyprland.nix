647df9f0abcd3e7d338cb9133aaab3de1d7c52cb
Scotty Loveless <scottyloveless@icloud.com>
2026-04-11 23:13:23 -0500

    hyprland catching up from old config

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -21,15 +21,54 @@
 
             general = {
                 gaps_in = 5;
-                gaps_out = 10;
+                gaps_out = 20;
                 border_size = 2;
+                
+                col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg;
+                col.inactive_border = rgba(595959aa)
+
+                resize_on_border = false;
+
+                allow_tearing = false;
+
+                layout = dwindle;
+
               };
+
+            decoartion = {
+
+              rounding = 10;
+              rounding_power = 2;
+
+              # Change transparency of focused and unfocused windows
+              active_opacity = 1.0;
+              inactive_opacity = 1.0;
+
+              shadow = {
+                  enabled = true;
+                  range = 4;
+                  render_power = 3;
+                  color = rgba(1a1a1aee);
+              };
+
+              # https://wiki.hypr.land/Configuring/Variables/#blur
+              blur = {
+                  enabled = false;
+                  size = 3;
+                  passes = 1;
+
+                  vibrancy = 0.1696;
+              };
+            };
 
             env = [
               "WLR_DRM_NO_ATOMIC,1"
               "WLR_RENDERER_ALLOW_SOFTWARE,1"
               "__GLX_VENDOR_LIBRARY_NAME,mesa"
               "LIBGL_ALWAYS_SOFTWARE,1"
+
+              "XCURSOR_SIZE,24"
+              "HYPRCURSOR_SIZE,24"
             ];
           };
       };
