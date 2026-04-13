b28ddd9d269d13c5990d4f6d05705de69e69db34
Scotty Loveless <scottyloveless@icloud.com>
2026-04-13 11:55:47 -0500

    1password in common.nix

--- home/git.nix
+++ home/git.nix
@@ -1,12 +1,17 @@
-{ ... }:
+{ pkgs, lib, ... }:
 {
-    programs.git = {
-      enable = true;
-      settings = {
-        user.email = "scottyloveless@icloud.com";
-        user.name = "Scotty Loveless";
-        init.defaultBranch = "main";
-        pull.rebase = true;
-        };
-      };
-  }
+  programs.git = {
+    enable = true;
+    settings = {
+      user.email = "scottyloveless@icloud.com";
+      user.name = "Scotty Loveless";
+      init.defaultBranch = "main";
+      pull.rebase = true;
+    };
+    extraConfig = {
+      gpg.format = "ssh";
+      "gpg \"ssh\"".program = "${lib.getExe' pkgs._1password-gui "op-ssh-sign"}";
+      commit.gpgsign = true;
+    };
+  };
+}
--- modules/common.nix
+++ modules/common.nix
@@ -1,5 +1,21 @@
 { pkgs, ... }:
+let
+  onePassPath =
+    if pkgs.stdenv.isDarwin then
+      "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+    else
+      "~/.1password/agent.sock";
+in
 {
+  programs.ssh = {
+
+    enable = true;
+    extraConfig = ''
+      Host *
+          IdentityAgent ${onePassPath}
+    '';
+  };
+
   time.timeZone = "America/Chicago";
   i18n.defaultLocale = "en_US.UTF-8";
 
@@ -25,4 +41,11 @@
   environment.shells = with pkgs; [ zsh ];
 
   system.stateVersion = "25.11";
+
+  programs._1password.enable = true;
+  programs._1password-gui = {
+    enable = true;
+    polkitPolicyOwners = [ "mox" ];
+  };
+
 }
