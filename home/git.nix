5f53b340d46ee346986574ee0615cf19d73bfb94
scottyloveless <8176125+scottyloveless@users.noreply.github.com>
2026-04-13 15:58:28 -0500

    gh

--- home/git.nix
+++ home/git.nix
@@ -10,7 +10,7 @@
       gpg.format = "ssh";
       "gpg \"ssh\"".program = "${lib.getExe' pkgs._1password-gui "op-ssh-sign"}";
       commit.gpgsign = true;
-      user.signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPk7r3TG9FVIfnZffcmLCN/r4gpuWdcd2oUWDd7/7J1Q";
+      user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKjtx6MrUOVgMIlLxXjELyI1MT1qMvDGq/TviW0yIimS";
     };
   };
 }
