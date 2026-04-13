79a30ee98c595750de1cbe9a1b38dd9a1d4c8e6f
scottyloveless <8176125+scottyloveless@users.noreply.github.com>
2026-04-13 15:05:24 -0500

    git user signing key

--- home/git.nix
+++ home/git.nix
@@ -3,15 +3,14 @@
   programs.git = {
     enable = true;
     settings = {
-      user.email = "scottyloveless@icloud.com";
-      user.name = "Scotty Loveless";
+      user.email = "8176125+scottyloveless@users.noreply.github.com";
+      user.name = "scottyloveless";
       init.defaultBranch = "main";
       pull.rebase = true;
-    };
-    extraConfig = {
       gpg.format = "ssh";
       "gpg \"ssh\"".program = "${lib.getExe' pkgs._1password-gui "op-ssh-sign"}";
       commit.gpgsign = true;
+      user.signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPk7r3TG9FVIfnZffcmLCN/r4gpuWdcd2oUWDd7/7J1Q";
     };
   };
 }
