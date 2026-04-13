74290412ddef93b0c1f6455596a7e188c9fb13df
scottyloveless <8176125+scottyloveless@users.noreply.github.com>
2026-04-12 20:38:51 -0500

    ghostty open new window

--- hosts/sandy/configuration.nix
+++ hosts/sandy/configuration.nix
@@ -125,7 +125,7 @@
         cmd-alt-h = [ ];
         cmd-m = [ ];
 
-        alt-enter = ''exec-and-forget osascript -e 'tell application "Ghostty" to activate' -e 'tell application "System Events" to tell process "Ghostty" to keystroke "n" using command down' '';
+        alt-enter = "exec-and-forget osascript -e 'tell application \"Ghostty\" to new window'";
 
         alt-slash = "layout tiles horizontal vertical";
         alt-comma = "layout accordion horizontal vertical";
