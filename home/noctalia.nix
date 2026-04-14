11eb9af13f069c1c2ae762b7bc2f1600f71aa7e6
Scotty Loveless <8176125+scottyloveless@users.noreply.github.com>
2026-04-13 21:10:04 -0500

    noctalia settings

--- home/noctalia.nix
+++ home/noctalia.nix
@@ -5,11 +5,13 @@
     settings = {
       bar = {
         position = "top";
-        density = "compact";
       };
       general = {
         radiusRatio = 0.2;
       };
+      colorSchemes.predefinedScheme = "Tokyo Night";
+      location = "Chelsea, AL";
+      wallpaper.enabled = false;
     };
   };
 }
--- modules/common.nix
+++ modules/common.nix
@@ -32,4 +32,9 @@
     polkitPolicyOwners = [ "mox" ];
   };
 
+  networking.networkmanager.enable = true;
+  hardware.bluetooth.enable = true;
+  services.tuned.enable = true;
+  services.upower.enable = true;
+
 }
