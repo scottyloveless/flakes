914c19de938bc690c11ee386ba9bedc4a96ba267
Scotty Loveless <8176125+scottyloveless@users.noreply.github.com>
2026-04-15 14:09:47 -0500

    apple silicon binary cache to beau

--- hosts/beau/configuration.nix
+++ hosts/beau/configuration.nix
@@ -16,7 +16,14 @@
     ../../modules/linuxcommon.nix
     ../../modules/common.nix
   ];
-
+  nix.settings = {
+    extra-substituters = [
+      "https://nixos-apple-silicon.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nixos-apple-silicon.cachix.org-1:8psDu5SA5dAD7qA0zMy5UT292TxeEPzIz8VVEr2Js20="
+    ];
+  };
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = false;
