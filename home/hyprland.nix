a4a54eff81358449ef093a6dafc494a500403398
Scotty Loveless <8176125+scottyloveless@users.noreply.github.com>
2026-04-15 22:26:29 -0500

    bibata

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -1,5 +1,12 @@
 { ... }:
 {
+  home.pointerCursor = {
+    gtk.enable = true;
+    # x11.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic";
+    size = 16;
+  };
   services.hyprpaper = {
     enable = true;
     settings = {
