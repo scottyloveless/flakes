b22909b36cc9c22115e76dc8067ba08706cd122b
Scotty Loveless <8176125+scottyloveless@users.noreply.github.com>
2026-04-24 10:14:53 -0500

    jankyborders hidpi and above style

--- modules/darwincommon.nix
+++ modules/darwincommon.nix
@@ -48,8 +48,8 @@
     enable = true;
     active_color = "0xff2CC3DF";
     inactive_color = "0xff565F89";
-    hidpi = false;
-    width = 4.0;
+    hidpi = true;
+    width = 5.0;
     order = "above";
   };
 
